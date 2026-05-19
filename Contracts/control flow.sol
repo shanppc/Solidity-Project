e7aef7d4b90cb030a419818577f704c41dfac185
Zeeshan Qureshi <pmz5164300@gmail.com>
2026-05-19 14:02:06 +0500

    style enchanced

--- Contracts/control flow.sol
+++ Contracts/control flow.sol
@@ -4,9 +4,11 @@ pragma solidity ^0.8.0;
 contract flow{
 
     address public Owner;
-constructor() payable {
-Owner = msg.sender;
-}
+    error InsufficientBalance(uint requested, uint avaliable);
+    error NotOwner(address caller);
+
+  constructor() payable {
+     Owner = msg.sender; }
 
     function CheckWithdraw(uint _amount) view public returns(string memory){
         if(_amount <= address(msg.sender).balance){
@@ -17,12 +19,12 @@ Owner = msg.sender;
         }
     } 
 
-error InsufficientBalance(uint requested, uint avaliable);
-function withdraw(uint amount) public view {
+     
+      function withdraw(uint amount) public view {
     if(amount > address(msg.sender).balance){
         revert InsufficientBalance(amount, address(msg.sender).balance);
     }
-}
+     }
 
 function subtractArray(uint[] memory numbers) public pure returns(uint) {
     uint result = 100;
@@ -32,15 +34,14 @@ function subtractArray(uint[] memory numbers) public pure returns(uint) {
     return result;
 }
 
-function checkage(uint _age) public pure returns(string memory){
-if (_age>= 18){
-    return "you are an Adult";
-}
-else {
-    return "you are underage";
-}
-}
-function sumEven(uint[] memory numbers) public pure returns(uint) {
+   function checkage(uint _age) public pure returns(string memory){
+   if (_age>= 18){
+    return "you are an Adult";}
+    else {
+    return "you are underage";}
+   }
+
+ function sumEven(uint[] memory numbers) public pure returns(uint) {
     uint result = 0;
     for(uint i = 0; i < numbers.length; i++) {
         if(numbers[i] % 2 == 0) {
@@ -48,15 +49,13 @@ function sumEven(uint[] memory numbers) public pure returns(uint) {
         }
     }
     return result;
-}
+  }
 
 
-error NotOwner(address caller);
 
-function onlyOwnerAction() public view  {
+
+    function onlyOwnerAction() public view  {
     if(msg.sender!= Owner){
-revert NotOwner(msg.sender);
-
-    }
-}
+   revert NotOwner(msg.sender);}
+     }
 }
